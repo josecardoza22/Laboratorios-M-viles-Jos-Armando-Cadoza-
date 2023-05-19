e46654b5047c75fad19dab8090b78ec984b9ef13
¨josecardoza22¨ <00089821@uca.edu.sv>
2023-05-19 16:14:10 -0600

    :memo: Laboratorio 9

---
+++ LaboAPP/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.AppLabo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    companion object {
+        const val APP_TAG = "APP_TAG"
+    }
+}
